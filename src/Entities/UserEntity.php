256f7bff72ae59868ef08af643fa3361e7cad83e
yangshanglin <24988493@qq.com>
2016-10-06 14:30:00 +0800

    first commit

---
+++ src/Entities/UserEntity.php
@@ -0,0 +1,10 @@
+<?php
+namespace Kof\OAuth2\Entities;
+
+use League\OAuth2\Server\Entities\UserEntityInterface;
+use League\OAuth2\Server\Entities\Traits\EntityTrait;
+
+class UserEntity implements UserEntityInterface
+{
+    use EntityTrait;
+}
